ed066e68a8b4cfb2f754806968adcf403695b670
tedmlkskywalker <luffylecrack@gmail.com>
2025-12-11 11:48:21 +0100

    Update page_admin.php

--- atelier3/page_admin.php
+++ atelier3/page_admin.php
@@ -1,9 +1,10 @@
 <?php
+// Démarrer la session
 session_start();
 
-// Vérifier que l'utilisateur est BIEN un "user"
-if (!isset($_SESSION['loggedin'], $_SESSION['username']) || $_SESSION['username'] !== 'user') {
-    header('Location: index.php');
+// Vérifier que l'utilisateur est BIEN "admin"
+if (!isset($_SESSION['loggedin'], $_SESSION['username']) || $_SESSION['username'] !== 'admin') {
+    header('Location: index.php');  // Redirection si l’utilisateur n’est pas admin
     exit();
 }
 ?>
@@ -11,11 +12,11 @@ if (!isset($_SESSION['loggedin'], $_SESSION['username']) || $_SESSION['username'
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Espace Utilisateur</title>
+    <title>Espace Administrateur</title>
 </head>
 <body>
 
-    <h1>Bienvenue sur la page utilisateur de l'atelier 3</h1>
+    <h1>Bienvenue sur la page administrateur de l'atelier 3</h1>
 
     <p>Vous êtes connecté en tant que : 
         <strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong>
