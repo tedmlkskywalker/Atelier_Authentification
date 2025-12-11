fc862af305701143525cd2def71519a5e66898ce
tedmlkskywalker <luffylecrack@gmail.com>
2025-12-11 11:43:49 +0100

    Update page_admin.php

--- atelier3/page_admin.php
+++ atelier3/page_admin.php
@@ -1,24 +1,27 @@
 <?php
-// Démarrer la session
 session_start();
 
-// Vérifier si l'utilisateur s'est bienconnecté
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header('Location: index.php'); // Dans le cas contraire, l'utilisateur sera redirigé vers la page de connexion
+// Vérifier que l'utilisateur est BIEN un "user"
+if (!isset($_SESSION['loggedin'], $_SESSION['username']) || $_SESSION['username'] !== 'user') {
+    header('Location: index.php');
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page protégée</title>
+    <title>Espace Utilisateur</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page administrateur de l'atelier 3</h1>
-    <p>Vous êtes connecté en tant que : <?php echo htmlspecialchars($_SESSION['username']); ?></p>
+
+    <h1>Bienvenue sur la page utilisateur de l'atelier 3</h1>
+
+    <p>Vous êtes connecté en tant que : 
+        <strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong>
+    </p>
+
     <a href="logout.php">Se déconnecter</a>
+
 </body>
 </html>
